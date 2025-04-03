a8ba44f325c523febf66d7714e2e41c654cc40a7
CRISTIANSILVAP <cristian.sperilla@mail.escuelaing.edu.co>
2025-04-02 19:02:34 -0500

    LAST

--- src/main/java/edu/eci/cvds/project/config/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/project/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/**").permitAll()
+                .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
